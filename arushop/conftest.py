7f2603bda68fe18f2bd4d008f92ab22012a9f9df
Mohammadreza Amani <More.amani@yahoo.com>
2024-01-12 20:17:38 +0330

    initial commit

---
+++ arushop/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from arushop.users.models import User
+from arushop.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()

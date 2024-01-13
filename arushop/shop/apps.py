3b8c28fd3ad41a027b2c5a5918a8992081122ed7
Mohammadreza Amani <More.amani@yahoo.com>
2024-01-14 02:17:10 +0330

    change app name to `"arushop.shop"`

--- arushop/shop/apps.py
+++ arushop/shop/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ShopConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "shop"
+    name = "arushop.shop"
+    verbose_name = _("Shops")

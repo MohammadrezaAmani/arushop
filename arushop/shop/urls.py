6b69dbff239db0cb2573e67dfb6cd7067d11df94
Mohammadreza Amani <More.amani@yahoo.com>
2024-01-14 02:17:42 +0330

    add api router of `[AddressViewSet, CartItemViewSet, CartViewSet, CategoryViewSet, ProductViewSet]`

---
+++ arushop/shop/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import AddressViewSet, CartItemViewSet, CartViewSet, CategoryViewSet, ProductViewSet
+
+app_name = "shop"
+
+router = routers.DefaultRouter()
+router.register(r"product", ProductViewSet, basename="product")
+router.register(r"address", AddressViewSet, basename="address")
+router.register(r"cart", CartViewSet, basename="cart")
+router.register(r"cartitem", CartItemViewSet, basename="cartitem")
+router.register(r"category", CategoryViewSet, basename="category")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]

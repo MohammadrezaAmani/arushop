f3c9847f247fd149e219dabb601112bb7f188ff6
Mohammadreza Amani <More.amani@yahoo.com>
2024-01-14 02:16:36 +0330

    add models to admin: `[Address, Cart, CartItem, Category, Product]`

--- arushop/shop/admin.py
+++ arushop/shop/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Address, Cart, CartItem, Category, Product
+
+admin.site.register(Address)
+admin.site.register(Cart)
+admin.site.register(CartItem)
+admin.site.register(Category)
+admin.site.register(Product)

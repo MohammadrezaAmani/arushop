fede9206baf9f0fab38767e4359a4b22b64e5195
Mohammadreza Amani <More.Amani@yahoo.com>
2024-01-14 10:35:58 +0330

    add recommandation app

--- arushop/recommandation_system/apps.py
+++ arushop/recommandation_system/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RecommandationSystemConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "recommandation_system"
+    name = "arushop.recommandation_system"

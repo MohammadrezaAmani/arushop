afefee475fc0d711616cfa309a100371ad004375
Mohammadreza Amani <More.Amani@yahoo.com>
2024-01-21 16:34:28 +0330

    minor chamges

--- middleware/auth/auth.py
+++ middleware/auth/auth.py
@@ -1,28 +1,33 @@
-import rest_framework_simplejwt
+import rest_framework_simplejwt.authentication
 from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.exceptions import InvalidToken
 
 
-def get_user(request):
-    token = request.META.get("HTTP_AUTHORIZATION", None)
-    if not token:
-        return AnonymousUser()
-    try:
-        token = token.split(" ")[1]
-        token = rest_framework_simplejwt.authentication.JWTAuthentication().get_validated_token(token)
-    except:
-        return AnonymousUser()
-    try:
-        user = rest_framework_simplejwt.authentication.JWTAuthentication().get_user(token)
-    except rest_framework_simplejwt.exceptions.InvalidToken:
-        return AnonymousUser()
-    return user
-
-
-class AuthMiddleware:
-    def __init__(self, get_response, *args, **kwargs):
+class JWTAuthenticationMiddleware:
+    def __init__(self, get_response=None):
         self.get_response = get_response
 
-    def __call__(self, request, *args, **kwargs):
-        request.user = get_user(request)
-        response = self.get_response(request)
-        return response
+        def __call__(self, request):
+            if request.path.startswith("/admin/"):
+                return self.get_response(request)
+
+            user, auth_token = self.authenticate(request)
+            request.user = user
+            request.auth = auth_token
+            response = self.get_response(request)
+            return response
+
+    def authenticate(self, request):
+        token = request.META.get("HTTP_AUTHORIZATION", None)
+
+        if not token:
+            return AnonymousUser(), None
+
+        try:
+            token = token.split(" ")[1]
+            validated_token = rest_framework_simplejwt.authentication.JWTAuthentication().get_validated_token(token)
+            user = rest_framework_simplejwt.authentication.JWTAuthentication().get_user(validated_token)
+        except InvalidToken:
+            return AnonymousUser(), None
+
+        return user, validated_token
